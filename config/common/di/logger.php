474ebbc70adfbb975bbfd5f779e5a9d1d89de2ea
Rahmat Syaparudin <rsyaparudin.developer@gmail.com>
2026-02-09 12:35:59 +0700

    streamRows

--- config/common/di/logger.php
+++ config/common/di/logger.php
@@ -14,12 +14,23 @@ use Yiisoft\Log\Target\File\FileTarget;
 // @var array $params
 
 return [
+    'log.target.security' => [
+        'class' => FileTarget::class,
+        '__construct()' => [
+            'file' => '@runtime/logs/security/security.log',
+            'categories' => ['security'],
+            'maxFileSize' => 1024,
+            'maxLogFiles' => 5,
+        ],
+    ],
+
     LoggerInterface::class => [
         'class' => Logger::class,
         '__construct()' => [
             'targets' => ReferencesArray::from([
                 FileTarget::class,
                 StreamTarget::class,
+                'log.target.security',
             ]),
         ],
     ],
