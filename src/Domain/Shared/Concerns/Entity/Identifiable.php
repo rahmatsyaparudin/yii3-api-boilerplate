1211edc073f57172472075ec0b5386a5d40178e0
Rahmat Syaparudin <rsyaparudin.developer@gmail.com>
2026-02-09 13:24:58 +0700

    migration

--- src/Domain/Shared/Concerns/Entity/Identifiable.php
+++ src/Domain/Shared/Concerns/Entity/Identifiable.php
@@ -1,143 +1,73 @@
 <?php
 
-
-
 declare(strict_types=1);
-
-
 
 namespace App\Domain\Shared\Concerns\Entity;
 
-
-
 // Domain Layer
-
 use App\Domain\Shared\ValueObject\Message;
 
-
-
 // Shared Layer
-
 use App\Shared\Exception\BadRequestException;
 
-
-
 trait Identifiable
-
 {
-
     protected string $resource;
-
     private ?int $syncMdb = null; // null = synced, 1 = not synced
 
-
-
     public function getId(): ?int
-
     {
-
         return $this->id;
-
     }
-
-
 
     public function getName(): string
-
     {
-
         return $this->name;
-
     }
-
-
 
     public function getResource(): string
-
     {
-
         return static::RESOURCE;
-
     }
 
-
-
     public function changeName(string $newName): void
-
     {
-
         $newName = trim($newName);
 
         if ($this->name === $newName) {
-
             return;
-
         }
-
-
 
         $this->validateName($newName);
-
         $this->name = $newName;
-
     }
-
-
 
     protected function validateName(string $name): void
-
     {
-
         if (empty($name)) {
-
             throw new BadRequestException(
-
                 translate: new \App\Shared\ValueObject\Message(
-
                     key: 'validation.name_required',
-
                     domain: 'validation',
-
                     params: ['resource' => $this->getResource()]
-
                 )
-
             );
-
         }
-
     }
-
-
 
     public function getSyncMdb(): ?int
-
     {
-
         return $this->syncMdb;
-
     }
-
-
 
     public function setSyncMdb(int $syncMdb): self
-
     {
-
         $this->syncMdb = $syncMdb;
-
         return $this;
-
     }
-
-
 
     public function updateSyncMdb(?int $syncMdb): void
-
     {
-
         $this->syncMdb = $syncMdb;
-
     }
-
 }
